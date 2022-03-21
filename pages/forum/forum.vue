e102ea9e76c19c768da90739152af7cc9ee0ae0c
IKBerry <416670297@qq.com>
2022-03-21 15:33:09 +0800

    完成vant组件库导入以及tabbar功能的开发

---
+++ pages/forum/forum.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    Forum
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
